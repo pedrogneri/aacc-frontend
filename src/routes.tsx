c241babb17a27f7e07de37b40b6625798b24caa5
Pedro Neri <pedrogneri@gmail.com>
2021-09-11 19:40:44 -0300

    ajusta css global

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,13 @@ import {
   BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 
-import { Home } from './views';
+import { Home, Login } from './views';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </Router>
 );
