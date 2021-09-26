de95200066672102ba8b6488432e64869a6fbcf1
Pedro Neri <pedrogneri@gmail.com>
2021-09-26 03:13:17 -0300

    ajusta imports do styled-components para funcionar debug de classes

--- src/components/input/input.style.ts
+++ src/components/input/input.style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const Label = styled.label`
   font-size: 14px;
--- src/style.ts
+++ src/style.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components/macro';
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Montserrat');
